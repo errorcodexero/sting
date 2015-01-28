4c6581949db81bf5cebb66c78091528bd0b0d55d
Adrian-Chimera <earthvexingtimelord@gmail.com>
2015-01-27 19:01:55 -0800

    fixed typos

--- lift_sim.cpp
+++ lift_sim.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
 using namestace std;
 
-float getTorqueMotor(float weightOunce, float sprocRad, float geerRed){
-	return (weightOunce*sprocRad)/geerRed);
+float getTorqueMotor(float weightOunce, float sprocRad, float gearRed){
+	return (weightOunce*sprocRad)/gearRed);
 }
 
-float weightSpeed(float speed, float geerRed, float stallTorque, float motorTorque, float sprocRad)
+float weightSpeed(float speed, float gearRed, float stallTorque, float motorTorque, float sprocRad)
 	int slope = speed/stallTorque;
-	return ((((slope*motorTorque)+speed)/60)*sprocRad)/(12*geerRed);
+	return ((((slope*motorTorque)+speed)/60)*sprocRad)/(12*gearRed);
 }
 
 int main(){
-	float geerRed = 250;
+	float gearRed = 250;
 	float sprocRad = 1+(11/16);
 	float stallTorque = 61;
 	float weight;
 	float speed;
-	float motorTorque = getTorqueMotor(weight, sprocRad, geerRad);
+	float motorTorque = getTorqueMotor(weight, sprocRad, gearRad);
 }

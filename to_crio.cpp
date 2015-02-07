dc5afa6dbe68adf8147b8a79a4de8869b37267fd
Logan Traffas <logan.traffas@gmail.com>
2015-02-07 14:36:47 -0800

    Oops

--- to_crio.cpp
+++ to_crio.cpp
@@ -384,8 +384,8 @@ public:
 			if(r) error_code|=512;*/
 		}
 		test.Set(1);
-		test1.Set(out.talon_srx_output[0]);
-		test2.Set(out.talon_srx_output[1]);
+		test1.Set(out.talon_srx[0]);
+		test2.Set(out.talon_srx[1]);
 		{
 			/*DriverStation *ds=DriverStation::GetInstance();
 			if(ds){

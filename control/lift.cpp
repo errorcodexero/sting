648859f951da74810eeda481d45b1194f75182f4
Logan Traffas <logan.traffas@gmail.com>
2015-02-15 21:31:02 -0800

    Added some safety for the presets and labled an important double

--- control/lift.cpp
+++ control/lift.cpp
@@ -211,7 +211,7 @@ Lift::Status status(Lift::Status_detail const& a){
 }
 
 Lift::Output control(Lift::Status_detail const& /*status*/,Lift::Goal const& goal){
-	const double POWER=-0.45;
+	const double POWER=-0.45;//The sign of this variable changes which direction the lifters go
 	if(goal.mode()==Lift::Goal::Mode::UP) return  POWER; 
 	if(goal.mode()==Lift::Goal::Mode::DOWN) return -POWER;
 	return 0.0;

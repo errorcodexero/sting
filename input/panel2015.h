e0b60d332554dc03f0ae301478e70997722e2165
Adrian-Chimera <earthvexingtimelord@gmail.com>
2015-02-24 19:47:41 -0800

    worked in panel 2015 h

--- input/panel2015.h
+++ input/panel2015.h
@@ -10,12 +10,22 @@ struct Panel{
 		DO_NOTHING,MOVE,FULL_RUN
 	};
 	Auto_mode auto_mode;
+    //Buttons:
+    bool current_collect;
+    bool current_drop;
+    bool move_drop;
+    bool move_collect;
+    bool chute_collect;
 	bool lifter_off;
     bool piston_aligner;
+    bool kicker_activate;
     //bool piston_switch_b;
     bool level_buttons [5];
+    //3 position Switches:
 	int move_arm_one;
     int move_arm_cont;
+    int collect_mode;
+    int drop_mode;
     
     
 	

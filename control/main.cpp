776b76194c3f4cf4341258ffd60153049325032a
Logan Traffas <logan.traffas@gmail.com>
2015-03-07 20:35:20 -0800

    WORKING! READY FOR MERGE WITH THE MASTER BRANCH! YAY :)

--- control/main.cpp
+++ control/main.cpp
@@ -27,7 +27,8 @@ bool in_range(T a,T b,T c){
 }
 
 Lift::Goal tote_lifter(Lift_position& tote_lift_pos,float ENGAGE_KICKER_HEIGHT,Main::Sticky_tote_goal pre_sticky_tote_goal,Posedge_toggle& piston,bool kick_and_lift=1){//Auto kicking code 
-	if(kick_and_lift && pre_sticky_tote_goal==Main::Sticky_tote_goal::ENGAGE_KICKER && !piston.get() && find_height(tote_lift_pos)[2]>=ENGAGE_KICKER_HEIGHT+3) piston.update(1);
+	static const float ABOVE_ENGAGE_KICKER_HEIGHT=ENGAGE_KICKER_HEIGHT+3;
+	if(kick_and_lift && pre_sticky_tote_goal==Main::Sticky_tote_goal::ENGAGE_KICKER && !piston.get() && find_height(tote_lift_pos)[2]>=ABOVE_ENGAGE_KICKER_HEIGHT) piston.update(1);
 	return Lift::Goal::go_to_height(std::array<double,3>{find_height(tote_lift_pos)[0],find_height(tote_lift_pos)[1],find_height(tote_lift_pos)[2]});
 }
 

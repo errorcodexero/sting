1d1c1ecf97f49227a8fddbd82ac4f435d904f1f5
Logan Traffas <logan.traffas@gmail.com>
2015-03-18 20:10:26 -0700

    Made panel2015 do something for its test

--- input/panel2015.cpp
+++ input/panel2015.cpp
@@ -1,4 +1,5 @@
 #include "panel2015.h"
+#include <iostream>
 #include <stdlib.h> 
 #include "util.h"
 #include "../util/util.h"
@@ -25,28 +26,66 @@ Panel::Panel():
 	drop_mode(0)
 {}
 
-ostream& operator<<(ostream& o, Panel::Auto_mode a){
-	if(a==Panel::Auto_mode::DO_NOTHING)o<<"Auto-Mode:DO_NOTHING ";
-	if(a==Panel::Auto_mode::MOVE)o<<"Auto-Mode:MOVE ";
-	if(a==Panel::Auto_mode::FULL_RUN)o<<"Auto-Mode:FULL_RUN ";
+ostream& operator<<(ostream& o,Panel::Auto_mode a){
+	o<<" Auto_Mode(";
+	if(a==Panel::Auto_mode::DO_NOTHING)o<<"do_nothing";
+	if(a==Panel::Auto_mode::MOVE)o<<"move";
+	if(a==Panel::Auto_mode::FULL_RUN)o<<"full_run";
+	o<<")";
 	return o;
 }
 
-ostream& operator<<(ostream& o, Panel::Level_buttons a){
-	o<<"Level:";
-	if(a==Panel::Level_buttons::LEVEL0)o<<"0 ";
-	if(a==Panel::Level_buttons::LEVEL1)o<<"1 ";
-	if(a==Panel::Level_buttons::LEVEL2)o<<"2 ";
-	if(a==Panel::Level_buttons::LEVEL3)o<<"3 ";
-	if(a==Panel::Level_buttons::LEVEL4)o<<"4 ";
-	if(a==Panel::Level_buttons::LEVEL5)o<<"5 ";	
-	if(a==Panel::Level_buttons::LEVEL6)o<<"6 ";
+ostream& operator<<(ostream& o,Panel::Level_buttons a){
+	o<<" Level(";
+	if(a==Panel::Level_buttons::LEVEL0)o<<"0";
+	if(a==Panel::Level_buttons::LEVEL1)o<<"1";
+	if(a==Panel::Level_buttons::LEVEL2)o<<"2";
+	if(a==Panel::Level_buttons::LEVEL3)o<<"3";
+	if(a==Panel::Level_buttons::LEVEL4)o<<"4";
+	if(a==Panel::Level_buttons::LEVEL5)o<<"5";	
+	if(a==Panel::Level_buttons::LEVEL6)o<<"6";
+	o<<")";
 	return o;
 }
-/*ostream& operator<<(ostream& o, Panel::Slide_pos a){
-	o<<"Arm Slider="<<a*20<<"% ";
+
+enum class Operation_buttons{
+		COLLECT_CURRENT,DROP_CURRENT,MOVE_COLLECT,MOVE_DROP
+	};
+ostream& operator<<(ostream& o,Panel::Operation_buttons a){
+	o<<" Operation_buttons(";
+	if(a==Panel::Operation_buttons::COLLECT_CURRENT)o<<"collect_current";
+	if(a==Panel::Operation_buttons::DROP_CURRENT)o<<"drop_current";
+	if(a==Panel::Operation_buttons::MOVE_COLLECT)o<<"move_collect";
+	if(a==Panel::Operation_buttons::MOVE_DROP)o<<"move_drop";
+	o<<")";
 	return o;
-}*/
+}
+
+ostream& operator<<(ostream& o,Panel p){
+	o<<"Panel(";
+	o<<" "<<p.auto_mode;
+	o<<", "<<p.level_buttons;
+	o<<", "<<p.operation_buttons;
+	o<<", slide_pos:"<<p.slide_pos;
+	o<<", Buttons(";
+	o<<" move_arm_to_pos:"<<p.move_arm_to_pos;
+	o<<", collect_current:"<<p.current_collect;
+	o<<", current_drop:"<<p.current_drop;
+	o<<", move_drop:"<<p.move_drop;
+	o<<", move_collect:"<<p.move_collect;
+	o<<", chute_collect:"<<p.chute_collect;
+	o<<", lifter_off:"<<p.lifter_off;
+	o<<", piston_aligner:"<<p.piston_aligner;
+	o<<", kicker_activate:"<<p.kicker_activate<<")";
+	o<<", 3_pos_switches(";
+	o<<" target_type:"<<p.target_type;
+	o<<", move_arm_one"<<p.move_arm_one;
+	o<<", move_arm_cont"<<p.move_arm_cont;
+	o<<", collect_mode"<<p.collect_mode;
+	o<<", drop_mode"<<p.drop_mode<<")";
+	o<<")";
+    return o;
+}
 
 Panel::Auto_mode automodeconvert(int potin){
 	if(potin==0)return Panel::Auto_mode::DO_NOTHING;
@@ -64,22 +103,36 @@ Panel interpret(Driver_station_input d){
 	}
 	{
 	//Operation Buttons
-	float op=d.analog[2];
-	if(op<=1&&op>=.5)panel.level_buttons=Panel::Level_buttons::LEVEL0;
-	if(op>1&&op<=1.8)panel.level_buttons=Panel::Level_buttons::LEVEL1;
-	if(op>1.8&&op<=2.5)panel.level_buttons=Panel::Level_buttons::LEVEL2;
-	if(op>2.5&&op<=3)panel.level_buttons=Panel::Level_buttons::LEVEL3;
-	if(op>3&&op<=3.8)panel.level_buttons=Panel::Level_buttons::LEVEL4;
-	if(op>3.8&&op<4.5)panel.level_buttons=Panel::Level_buttons::LEVEL5;
-	if(op>4.5&&op<5)panel.level_buttons=Panel::Level_buttons::LEVEL6; 
-    else panel.level_buttons=panel.level_buttons;
+	float op=d.analog[1];
+	if(op<=1&&op>=.5)panel.level_buttons=Panel::Level_buttons::LEVEL0;//-.75
+	else if(op>1&&op<=1.8)panel.level_buttons=Panel::Level_buttons::LEVEL1;//-.5
+	else if(op>1.8&&op<=2.5)panel.level_buttons=Panel::Level_buttons::LEVEL2;//-.25
+	else if(op>2.5&&op<=3)panel.level_buttons=Panel::Level_buttons::LEVEL3;//0
+	else if(op>3&&op<=3.8)panel.level_buttons=Panel::Level_buttons::LEVEL4;//.32
+	else if(op>3.8&&op<4.5)panel.level_buttons=Panel::Level_buttons::LEVEL5;//.65
+	else if(op>4.5&&op<5)panel.level_buttons=Panel::Level_buttons::LEVEL6;//1
 	}
 	return panel;
 }
 
+Driver_station_input driver_station_input_rand(){//Copied over from hammer. Adrian update this if needed.
+	Driver_station_input r;
+	for(unsigned i=0;i<r.ANALOG_INPUTS;i++){
+		r.analog[i]=(0.0+rand()%101)/100;
+	}
+	for(unsigned i=0;i<r.DIGITAL_INPUTS;i++){
+		r.digital[i]=rand()%2;
+	}
+	return r;
+}
+
 #ifdef PANEL2015_TEST
 int main(){
-
+	Panel p;
+	for(unsigned i=0;i<50;i++){
+		interpret(driver_station_input_rand());
+	}
+	cout<<p<<"\n";
 	return 0;
 }
 #endif
--- input/panel2015.h
+++ input/panel2015.h
@@ -8,41 +8,34 @@
 #include "../control/combo_lift.h"
 
 struct Panel{
-	enum class Auto_mode{
-		DO_NOTHING,MOVE,FULL_RUN
-	};
+	enum class Auto_mode{DO_NOTHING,MOVE,FULL_RUN};
 	Auto_mode auto_mode;
-	enum class Level_buttons{
-		LEVEL0,LEVEL1,LEVEL2,LEVEL3,LEVEL4,LEVEL5,LEVEL6
-	};
+	enum class Level_buttons{LEVEL0,LEVEL1,LEVEL2,LEVEL3,LEVEL4,LEVEL5,LEVEL6};
 	Level_buttons level_buttons;
-	enum class Operation_buttons{
-		COLLECT_CURRENT,DROP_CURRENT,MOVE_COLLECT,MOVE_DROP
-	};
+	enum class Operation_buttons{COLLECT_CURRENT,DROP_CURRENT,MOVE_COLLECT,MOVE_DROP};
 	Operation_buttons operation_buttons;
 	float slide_pos;
     //Buttons:
-        bool move_arm_to_pos;
-        bool current_collect;
-        bool current_drop;
-        bool move_drop;
-        bool move_collect;
-        bool chute_collect;
-        bool lifter_off;
-        bool piston_aligner;
-        bool kicker_activate;
+	bool move_arm_to_pos;
+	bool current_collect;
+	bool current_drop;
+	bool move_drop;
+	bool move_collect;
+	bool chute_collect;
+	bool lifter_off;
+	bool piston_aligner;
+	bool kicker_activate;
     //3 position Switches:
-        int target_type;
-        int move_arm_one;
-        int move_arm_cont;
-        int collect_mode;
-        int drop_mode;
+	int target_type;
+	int move_arm_one;
+	int move_arm_cont;
+	int collect_mode;
+	int drop_mode;
     	
 	Panel();
 };
 std::ostream& operator<<(std::ostream&,Panel::Auto_mode);
 std::ostream& operator<<(std::ostream&,Panel::Level_buttons);
-//std::ostream& operator<<(std::ostream&,Panel::Slide_pos);
 std::ostream& operator<<(std::ostream&,Panel);
 
 Panel interpret(Driver_station_input);

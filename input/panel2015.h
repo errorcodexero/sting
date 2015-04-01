a8d4234f8902fd3c37fde4dfcf4456b83bc916d3
Matthew Macovsky <macovsky.m@gmail.com>
2015-03-31 19:19:23 -0700

    OI actually works for most things on it although the buttons do not do what the labels say

--- input/panel2015.h
+++ input/panel2015.h
@@ -13,8 +13,8 @@ struct Panel{
 	Auto_mode auto_mode;//Added
 	enum class Level_button{DEFAULT,LEVEL0,LEVEL1,LEVEL2,LEVEL3,LEVEL4,LEVEL5,LEVEL6};
 	Level_button level_button;//Added
-	enum class Operation_buttons{DEFAULT,KILL,ENGAGE_KICKER_HEIGHT,KICKER_ACTIVATE,MOVE_COLLECT,CAN_NUDGE};
-	Operation_buttons operation_buttons;
+	enum class Operation_buttons{DEFAULT,KILL,ENGAGE_KICKER_HEIGHT,KICKER_ACTIVATE,MOVE_COLLECT,CAN_NUDGE};//Added
+	Operation_buttons operation_buttons;//Added
 	float slide_pos;
 	float override_height;
     //Buttons:
